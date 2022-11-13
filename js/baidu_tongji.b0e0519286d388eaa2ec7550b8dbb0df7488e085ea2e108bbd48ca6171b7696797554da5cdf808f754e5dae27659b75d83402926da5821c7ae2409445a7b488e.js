090d396810d01b3b782b6d335bdbb1316ab05756
fatedeity <fatedeity@users.noreply.github.com>
2022-11-13 10:31:48 +0000

    deploy: fatedeity/hugo-blog@2d1189848959be17ae87ef4696a2383c7911c5b3

---
+++ js/baidu_tongji.b0e0519286d388eaa2ec7550b8dbb0df7488e085ea2e108bbd48ca6171b7696797554da5cdf808f754e5dae27659b75d83402926da5821c7ae2409445a7b488e.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?aa99b552002ea66dbd02af2665682e36";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
